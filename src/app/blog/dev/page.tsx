f4afd2c169f105a43ff59acc11afab48161d2496
衣笠みのる <168079463+minoru-kinugasa-105@users.noreply.github.com>
2024-09-06 14:19:01 +0900

    多少のレイアウトなどの調整によるオートcommit/push

--- src/app/blog/dev/page.tsx
+++ src/app/blog/dev/page.tsx
@@ -23,68 +23,6 @@ export default function Article() {
                 すげーよ、ほめてやるよ<br />
                 (ﾟ∀ﾟ)ｱﾋｬ
             </main>
-            {/* 
-            <section className="article">
-    <h1 className="article-title">
-        たいとる
-    </h1>
-    <div className="article-author">
-        <Image
-            src={`/images/blog/icon.png`}
-            width={50}
-            height={50}
-            alt="Author"
-            className="author-icon"
-        />
-        <span className="author-name">
-            おれ
-        </span>
-    </div>
-    <ul className="article-tag">
-        <li className="tag-green">
-            <Link href="#">
-                React
-            </Link>
-        </li>
-    </ul>
-    <hr />
-    <div className="article-content">
-
-        <h2>てすとBlog</h2><p>この記事は開発段階のBlogの記事となっております！<br />この記事は以下の技術を用いて開発されています。</p><ul><li>React (Next.js)</li><li>Github Pages</li></ul><p>また、このBlogではこの記事を読んでいる人が見やすいデザインにするために独自でデザイン・システムを作成しています。<br />デザインは、<a href="https://www.wowthemes.net/memoirs-free-jekyll-theme/">memoirs-free-jekyll-theme</a>を参考に作成いたしました！</p><p>また、ページの公開に使用しているGithub Pagesですが、Next.jsをデプロイする技術をGithubが提供していたため、それを利用しました。<span className="citation">参照元:<span className="blank"></span><a href="https://github.com/gregrickaby/nextjs-github-pages">https://github.com/gregrickaby/nextjs-github-pages</a></span><small><span className="cancellation"><small>もっとスマートな方法があった気がしてなりません…</small></span></small></p>
-
-    </div>
-
-    <hr />
-    <div className="article-footer">
-        <div className="footer-author">
-            <div className="author-headline">
-                <Image
-                    src={`/images/blog/icon.png`}
-                    width={50}
-                    height={50}
-                    alt="Author"
-                    className="headline-icon"
-                />
-                <div className="headline-link">
-                    <Link href="aaa">
-                        <FontAwesomeIcon icon={faTwitter} />
-                    </Link>
-                    <Link href="aaa">
-                        <FontAwesomeIcon icon={faEnvelope} />
-                    </Link>
-                </div>
-            </div>
-            <div className="author-intro">
-                <h4>
-                    いんとろ
-                </h4>
-                <p>
-                    ああああああああああああああああああああああああ
-                </p>
-            </div>
-        </div>
-    </div>
-</section> */}
 
             <Footer />
         </>
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -17,7 +17,7 @@ import Post from "@/types/PostType";
 async function getPosts(): Promise<Post[]> {
     const filePath = path.resolve(process.cwd(), 'data', 'posts.json');
     const jsonData = fs.readFileSync(filePath, 'utf-8');
-    return JSON.parse(jsonData);
+    return jsonData ? JSON.parse(jsonData) : undefined;
 }
 
 export default async function Blog() {
@@ -40,52 +40,65 @@ export default async function Blog() {
                     <div className="main blog-main">
 
                         <div className="blog-article">
-                            {posts.map((post, index) => (
-                                <article key={post.slug} className={`card ${index === 0 ? "big-card" : ""}`}>
-                                    <div className="card-imgbox">
-                                        <Image
-                                            src={`/images/blog/${post.thumbnail}`}
-                                            width={100}
-                                            height={80}
-                                            alt="article image"
-                                            className="card-img"
-                                        />
-                                    </div>
-                                    <Link href={`/blog/${post.slug}`} className="card-explanation">
-                                        <h2 className="card-title">
-                                            {post.title}
-                                        </h2>
-                                        <h3 className="card-desc">
-                                            {post.description}
-                                        </h3>
-                                    </Link>
-                                    <div className="card-footer">
-                                        <ul className="card-taglist">
-                                        {post.tag.map((tag: { name: string; color: string }) => (
-                                                <li key={tag.name} className={`card-tag ${tag.color}`}>
-                                                    <Link href={`/blog//tag/${tag.name}`}>
-                                                        {tag.name}
-                                                    </Link>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                        <span className="card-date">
-                                            {post.posted}
-                                        </span>
-                                    </div>
-                                </article>
-                            ))}
+                            {
+                                posts ? (
+                                    posts.map((post, index) => (
+                                        <article key={post.slug} className={`card ${index === 0 ? "big-card" : ""}`}>
+                                            <div className="card-imgbox">
+                                                <Image
+                                                    src={`/images/blog/${post.thumbnail}`}
+                                                    width={100}
+                                                    height={80}
+                                                    alt="article image"
+                                                    className="card-img"
+                                                />
+                                            </div>
+                                            <Link href={`/blog/${post.slug}`} className="card-explanation">
+                                                <h2 className="card-title">
+                                                    {post.title}
+                                                </h2>
+                                                <h3 className="card-desc">
+                                                    {post.description}
+                                                </h3>
+                                            </Link>
+                                            <div className="card-footer">
+                                                <ul className="card-taglist">
+                                                    {post.tag.map((tag: { name: string; color: string }) => (
+                                                        <li key={tag.name} className={`card-tag ${tag.color}`}>
+                                                            <Link href={`/blog/tag/${tag.name}`}>
+                                                                {tag.name}
+                                                            </Link>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                                <span className="card-date">
+                                                    {post.posted}
+                                                </span>
+                                            </div>
+                                        </article>
+                                    ))
+                                ) : ""
+                            }
                         </div>
+                        {
+                            posts ? "" : (
+                                <p className="blog-notfound">まだ記事はないようです、、、</p>
+                            )
+                        }
 
-                        <div className="blog-pagination">
-                            <Link href="">«</Link>
-                            <Link href="">1</Link>
-                            <Link href="">2</Link>
-                            <Link href="">3</Link>
-                            <Link href="">4</Link>
-                            <Link href="">5</Link>
-                            <Link href="">»</Link>
-                        </div>
+                        {/* {
+                            posts ? (
+                                <div className="blog-pagination">
+                                    <Link href="">«</Link>
+                                    <Link href="">1</Link>
+                                    <Link href="">2</Link>
+                                    <Link href="">3</Link>
+                                    <Link href="">4</Link>
+                                    <Link href="">5</Link>
+                                    <Link href="">»</Link>
+                                </div>
+                            ) : ""
+                        } */}
                     </div>
                 </section>
             </main>
