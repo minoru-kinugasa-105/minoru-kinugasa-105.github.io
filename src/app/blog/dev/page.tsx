f874763f47b4cf133a737b5c0c417b73216be803
衣笠みのる <168079463+minoru-kinugasa-105@users.noreply.github.com>
2024-09-18 20:29:47 +0900

    gitignoreの修正

--- src/app/blog/dev/page.tsx
+++ src/app/blog/dev/page.tsx
@@ -23,12 +23,46 @@ export default function Article() {
                 <span className="author-name">みのる</span>
             </div>
             <ul className="article-tag">
-                <li className="tag-green">
-                    <Link href="#">React</Link>
+                <li className="tag-gray">
+                    <Link href="#">Git</Link>
                 </li>
             </ul>
             <hr />
-            <div className="article-content"></div>
+
+
+
+
+
+
+
+            <div className="article-content">
+                <h1>
+                    ローカルでブランチを切らずに開発を進めてしまったときの話
+                </h1>
+                <p>
+                    こんにちは！！<br />
+                    今回は、ローカルでブランチを切らずに開発を進めてしまったとき、どうやってpushしたのかとその後どうしたかなどをゆるーく話したいとおもいます！<br />
+                </p>
+                <p>
+                    みなさんは、Git、GitHubを使った開発はされていますか？<br />
+                    僕はもうバリバリ使うんですが、先輩との共同開発でついにやらかしたと思いました... <br />
+                </p>
+                <h2>
+                    何が起こったのか
+                </h2>
+                <p>
+                    まず、新しい機能を作成するときにはブランチを分けて開発するのが基本ですよね<br />
+                    しかしこのとき何故か、疲れていたのかブランチを切らずにmasterブランチで開発を進めてしまいました... <br />
+                    <br />
+                    それが起きると何がまずいかというと、まずmasterブランチに
+                </p>
+            </div>
+
+
+
+
+
+
 
             <hr />
             <div className="article-footer">
