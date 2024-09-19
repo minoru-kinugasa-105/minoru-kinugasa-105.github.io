8b25b1ea3a5003c77b82c0af2ade66a62e6dea51
衣笠みのる <168079463+minoru-kinugasa-105@users.noreply.github.com>
2024-09-19 21:33:53 +0900

    developページ適正化

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -1,18 +1,14 @@
-import Image from 'next/image';
-import Link from 'next/link';
-
 import '@/css/object/component/section.scss';
-import '@/css/layout/develop.scss';
 
 export default function Developer() {
     return (
         <main>
-            <section className="section first-section develop">
+            <section className="section first-section">
                 <h1 className="section-title">
                     <span className="title-text">Develop</span>
                     <span className="subtitle-text">開発者がつかうところ</span>
                 </h1>
-                <div className="main develop-main">
+                <div className="main">
                     え、よくこのディレクトリ見つけましたね...
                     <br />
                     でもここは開発者が開発に使ってるところなので何もないです...() <br />
